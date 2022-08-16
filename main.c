95cbe9d359dd0688b3255d50fbac0bfb2ffddccd
Dembenjm <demianoyarzunmancilla@gmail.com>
2022-08-16 10:53:41 -0300

    Mov. enemigo, detector luz, puerta

--- main.c
+++ main.c
@@ -8,12 +8,15 @@
 #define M 20
 #define MAXBATERIAS 5
 void mover_personaje(); //función para mover el personaje
+void mover_enemigo();
 void cargar_bitmaps();
 void importar_mapa();
 void pintar_fondo();
 void generarbaterias();
 void agarrar_bateria();
+void agarrar_llave();
 void dibujar_personaje();
+void indicador_bateria();
 BITMAP *buffer; //se declara buffer como tipo bitmap, aquí se almacenarán las imagenes
 BITMAP *fondo; //se declara un bitmap para almacenar el archivo de imagen del personaje
 BITMAP *pared;
@@ -22,6 +25,10 @@ BITMAP *bateria;
 BITMAP *bateriabmp;
 BITMAP *luz;
 BITMAP *luzbmp;
+BITMAP *llave;
+BITMAP *llavebmp;
+BITMAP *puerta;
+BITMAP *puertabmp;
 struct personaje
 {
       int encendida;
@@ -29,6 +36,8 @@ struct personaje
       int py; //variable para mover al personaje en eje y
       int dir;
       int vel_pasos;
+      int vida;
+      int llave;
       BITMAP *personajebmp;
       BITMAP *personaje;
 }player;
@@ -52,12 +61,18 @@ int ventana_w=800;
 int ventana_h=600;
 int tiempo_luz;
 int cont_baterias=0;
+int estado_puerta=0;
+int detector_linterna=0;
 int main()
 {
       player.px=ventana_w/2;
       player.py=(ventana_h/2)+100;
+      player.vida=1;
+      player.llave=0;
       player.encendida=0;
       enemigo.activado=0;
+      estado_puerta=0;
+      detector_linterna=0;
       allegro_init();//macro que inicializa allegro
       install_keyboard();//instala el controlador de interrupciones del teclado allegro, se debe llamar a esta función antes de usar cualquiera de las rutinas de entrada de teclado
       set_color_depth(32);//establece la profundidad de color que se utilizará en las llamadas posteriores, profundidades validas: 8, 15 , 16, 24, 32bits
@@ -65,16 +80,20 @@ int main()
       cargar_bitmaps();
       importar_mapa(mapa);
       generarbaterias(mapa);
-      //VARIABLES PARA DIBUJAR UN RECTANGULO
       while (!key[KEY_ESC])//mientras la tecla que se presione sea distinta que esc se mantiene dentro del bucle y por lo tanto el juego se sigue ejecutando
       {
             mover_personaje();
             mover_enemigo();
-            rest(5);
-            clear_bitmap(buffer);
-            pintar_fondo(mapa);//imprime matriz con objetos del mapa
+            rest(1);
+            pintar_fondo();//imprime matriz con objetos del mapa
+            indicador_bateria();
             dibujar_personaje();
             blit(buffer,screen,0,0,0,0,SCREEN_W,SCREEN_H); //el buffer es dibujado en la pantalla
+            clear_bitmap(buffer);
+            if(mapa[(player.py+40)/40][player.px/40]==6)
+            {
+                  exit(-1);
+            }
       }
       destroy_bitmap(player.personaje);
       destroy_bitmap(player.personajebmp);
@@ -86,6 +105,8 @@ int main()
       destroy_bitmap(bateriabmp);
       destroy_bitmap(luz);
       destroy_bitmap(luzbmp);
+      destroy_bitmap(puerta);
+      destroy_bitmap(puertabmp);
       return 0;
 }
 END_OF_MAIN(); //allegro requiere que se le indique donde termina el main
@@ -99,12 +120,16 @@ void cargar_bitmaps()
       enemigo.enemigo=create_bitmap(40,40);
       pared=load_bitmap("pared.bmp",NULL);
       piso=load_bitmap("piso.bmp",NULL);
-      bateria=create_bitmap(40,40);
+     // bateria=create_bitmap(40,40);
       bateriabmp=load_bitmap("bateria.bmp",NULL);
       luz=create_bitmap(40,120);
       luzbmp=load_bitmap("luz.bmp",NULL);
       carga.carga=create_bitmap(120,40);
       carga.cargabmp=load_bitmap("cargabmp.bmp",NULL);
+      llave=create_bitmap(40,40);
+      llavebmp=load_bitmap("llave.bmp",NULL);
+      puerta=create_bitmap(40,40);
+      puertabmp=load_bitmap("puerta.bmp",NULL);
 }
 
 void importar_mapa()
@@ -134,15 +159,15 @@ void pintar_fondo()
       {
             for(j=0;j<M;j++)
             {
-                  if(mapa[i][j]==0)
+                  if(mapa[i][j]==0)//PISO
                   {
                         draw_sprite(buffer, piso, j*40, i*40);
                   }
-                  if(mapa[i][j]==1)
+                  if(mapa[i][j]==1)//PARED
                   {
                         draw_sprite(buffer, pared, j*40, i*40);
                   }
-                  if(mapa[i][j]==2)
+                  if(mapa[i][j]==2)//BATERIA
                   {
                         draw_sprite(buffer, piso, j*40, i*40);
                         draw_sprite(buffer, bateriabmp, j*40, i*40);
@@ -151,12 +176,12 @@ void pintar_fondo()
                   {
                         agarrar_bateria(mapa);
                   }
-                  if(mapa[i][j]==3)
+                  if(mapa[i][j]==3)//PISO DETECTOR
                   {
                         draw_sprite(buffer, piso, j*40, i*40);
                   }
-                  if(mapa[i][j]==4)
-                  {
+                  if(mapa[i][j]==4)//ENEMIGO
+                  {     //ACTIVAR ENEMIGO EN LAS COORDENADAS ACTUALES
                         if(mapa[player.py/40][player.px/40]==3&&enemigo.activado==0)
                         {
                               enemigo.activado=1;
@@ -165,41 +190,75 @@ void pintar_fondo()
                         }
                         draw_sprite(buffer, piso, j*40, i*40);
                   }
+                  if(mapa[i][j]==5)//LLAVE
+                  {
+                        draw_sprite(buffer, piso, j*40, i*40);
+                        if(mapa[(player.py+44)/40][player.px/40]==5||mapa[player.py/40][player.px/40]==5||mapa[player.py/40][(player.px+40)/40]==5)
+                        {
+                              agarrar_llave(mapa);
+                        }
+                        else
+                        {
+                              stretch_blit(llavebmp,llave,0,0,300,300,0,0,40,40);
+                              draw_sprite(buffer, llave, j*40, i*40);
+                        }
+                  }
+                  if(mapa[i][j]==6)//PUERTA
+                  {
+                        draw_sprite(buffer, piso, j*40, i*40);
+                        if(mapa[(player.py-4)/40][player.px/40]==6 && player.llave==1)//ABRE PUERTA
+                        {
+                              printf("estado_puerta=%d",estado_puerta);
+                              estado_puerta=1;
+                        }
+                        if(estado_puerta==0)//CERRADA
+                        {
+                              stretch_blit(puertabmp,puerta,0,0,40,40,0,0,40,40);
+                              draw_sprite(buffer,puerta,j*40,i*40);
+                        }
+                        if(estado_puerta==1)//ABIERTA
+                        {
+                              stretch_blit(puertabmp,puerta,40,0,40,40,0,0,40,40);
+                              draw_sprite(buffer,puerta,j*40,i*40);
+                        }
+                  }
             }
-            stretch_blit(carga.cargabmp,carga.carga,120*cont_baterias,0,720,40,0,0,720,40);
-            draw_sprite(buffer,carga.carga,670,10);
       }
 }
 
 void dibujar_personaje()
 {
-      stretch_blit(player.personajebmp, player.personaje, player.dir*300,0,300,300,0,0,40,40);
-      draw_sprite(buffer, player.personaje,player.px,player.py);
-      if(player.encendida==1)
+      if(player.vida!=0)
       {
-            if(tiempo_luz!=0)
+            stretch_blit(player.personajebmp, player.personaje, player.dir*300,0,300,300,0,0,40,40);
+            draw_sprite(buffer, player.personaje,player.px,player.py);
+            if(player.encendida==1)
             {
-                  tiempo_luz=tiempo_luz-1;
-                  printf("tiempo_luz=%d",tiempo_luz);
-                  if(player.dir==0) //ARRIBA
+                  if(tiempo_luz!=0)//LINTERNA ENCENDIDA
                   {
-                        triangle(buffer, player.px, player.py-120, player.px+20, player.py, player.px+40, player.py-120,makeacol(255,255,0,0));
+                        tiempo_luz=tiempo_luz-1;
+                        //printf("tiempo_luz=%d",tiempo_luz);
+                        if(player.dir==0) //ARRIBA
+                        {
+                              triangle(buffer, player.px, player.py-120, player.px+20, player.py, player.px+40, player.py-120,makeacol(255,255,0,0));
+                        }
+                        else if(player.dir==1) //IZQUIERDA
+                        {
+                              triangle(buffer, player.px-120, player.py+40, player.px, player.py+20, player.px-120, player.py,makeacol(255,255,0,0));
+                        }
+                        else if(player.dir==2)//ABAJO
+                        {
+                              triangle(buffer, player.px+40, player.py+160, player.px+20, player.py+40, player.px, player.py+160,makeacol(255,255,0,0));
+                        }
+                        else //DERECHA
+                        {
+                              triangle(buffer, player.px+160, player.py, player.px+40, player.py+20, player.px+160, player.py+40,makeacol(255,255,0,0));
+                        }
                   }
-                  else if(player.dir==1) //IZQUIERDA
-                  {
-                        triangle(buffer, player.px-120, player.py+40, player.px, player.py+20, player.px-120, player.py,makeacol(255,255,0,0));
-                  }
-                  else if(player.dir==2)//ABAJO
-                  {
-                        triangle(buffer, player.px+40, player.py+160, player.px+20, player.py+40, player.px, player.py+160,makeacol(255,255,0,0));
-                  }
-                  else //DERECHA
-                  {
-                        triangle(buffer, player.px+160, player.py, player.px+40, player.py+20, player.px+160, player.py+40,makeacol(255,255,0,0));
-                  }
+                  else{player.encendida=0;}
             }
-            else{player.encendida=0;}
       }
+      else{}
       if(enemigo.activado==1)
       {
             stretch_blit(enemigo.enemigobmp, enemigo.enemigo, enemigo.dir*300,0,300,300,0,0,40,40);
@@ -259,9 +318,21 @@ void mover_personaje()
                         player.vel_pasos=2;
                   }
             }
+            if(mapa[(player.py-4)/40][(player.px+10)/40]==6 && player.llave==1)
+            {
+                  player.py=player.py-player.vel_pasos;
+                  if(key[KEY_LSHIFT])
+                  {
+                        player.vel_pasos=4;
+                  }
+                  else
+                  {
+                        player.vel_pasos=2;
+                  }
+            }
             else{}
       }
-      else if(key[KEY_S]) //movimiento hacia la ABAJO cuando se presiona la tecla S
+      else if(key[KEY_S]) //movimiento hacia ABAJO cuando se presiona la tecla S
       {
             player.dir=2;
             if(mapa[(player.py+40)/40][player.px/40] != 1 && mapa[(player.py+40)/40][(player.px+40)/40] != 1)
@@ -305,6 +376,7 @@ void mover_personaje()
             printf("cont_baterias=%d\n",cont_baterias);
             key[KEY_SPACE]=false;
       }
+
 }
 
 void mover_enemigo()
@@ -312,23 +384,50 @@ void mover_enemigo()
       enemigo.vel_pasos=1;
       if(enemigo.activado==1)
       {
-            if(enemigo.px<player.px)
+            int distancia_y=(enemigo.py+40)/40-(player.py-120)/40;
+            int distancia_x=(enemigo.px+40)/40-(player.px-120)/40;
+            //printf("distancia_y=%d\n",distancia_y);
+            //printf("distancia_x=%d\n",distancia_x);
+            if(distancia_y>0&&distancia_y<4&&enemigo.px/40==player.px/40&&player.dir==0&&player.encendida==1) //DETECTAR LINTERNA HACIA ARRIBA
             {
-                  enemigo.px=enemigo.px+enemigo.vel_pasos;
+                  printf("linterna en enemigo");
             }
-            if(enemigo.px>player.px)
+            else if(distancia_y<7&&distancia_y>=4&&enemigo.px/40==player.px/40&&player.dir==2&&player.encendida==1) //DETECTAR LINTERNA HACIA ABAJO
             {
-                  enemigo.px=enemigo.px-enemigo.vel_pasos;
+                  printf("linterna en enemigo");
             }
-            if(enemigo.py<player.py)
+            else if(distancia_x>0&&distancia_x<4&&enemigo.py/40==player.py/40&&player.dir==1&&player.encendida==1) //DETECTAR LINTERNA HACIA LA IZQUIERDA
             {
-                  enemigo.py=enemigo.py+enemigo.vel_pasos;
+                  printf("linterna en enemigo");
             }
-            if(enemigo.py>player.py)
+            else if(distancia_x<7&&distancia_x>=4&&enemigo.py/40==player.py/40&&player.dir==1&&player.encendida==1) //DETECTAR LINTERNA HACIA LA DERECHA
             {
-                  enemigo.py=enemigo.py-enemigo.vel_pasos;
+                  printf("linterna en enemigo");
+            }
+            else
+            {
+                  if(enemigo.px<player.px)
+                  {
+                        enemigo.px=enemigo.px+enemigo.vel_pasos;
+                  }
+                  if(enemigo.px>player.px)
+                  {
+                        enemigo.px=enemigo.px-enemigo.vel_pasos;
+                  }
+                  if(enemigo.py<player.py)
+                  {
+                        enemigo.py=enemigo.py+enemigo.vel_pasos;
+                  }
+                  if(enemigo.py>player.py)
+                  {
+                        enemigo.py=enemigo.py-enemigo.vel_pasos;
+                  }
             }
       }
+//      if((enemigo.py+4)/40==player.py/40 && enemigo.px/40==player.px/40 || (enemigo.px-4)/40==player.px/40 && enemigo.py/40==player.py/40)
+//      {
+//            player.vida=0;
+//      }
 }
 
 void generarbaterias()
@@ -357,5 +456,23 @@ void agarrar_bateria()
             cont_baterias=cont_baterias+1;
       }
       printf("baterias=%d\n",cont_baterias);
+}
+
+void agarrar_llave()
+{
+      mapa[(player.py+44)/40][player.px/40]=0;
+      mapa[player.py/40][player.px/40]=0;
+      mapa[player.py/40][(player.px+40)/40]=0;
+      if(mapa[(player.py+44)/40][player.px/40]==0&&mapa[player.py/40][player.px/40]==0&&mapa[player.py/40][(player.px+40)/40]==0)
+      {
+            player.llave=1;
+      }
+      printf("llave=%d\n",player.llave);
+}
+
+void indicador_bateria()
+{
+      stretch_blit(carga.cargabmp,carga.carga,120*cont_baterias,0,720,40,0,0,720,40);
+      draw_sprite(buffer,carga.carga,670,10);
 }
 
